8f90f74be8bc2a9eebdf50563bcd58d85657db66
J3SS3HVA <jesse.leeuwerink@hva.nl>
2024-01-15 13:17:35 +0100

    0.55

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+ 
+// const scheduleButton = document.querySelector('#Schedule-button');
+
+// scheduleButton.addEventListener('click', function(){
+//     const scheduleSection = document.querySelector('main section:nth-child(2)');
+//     scheduleSection.classList.toggle('show-Schedule');
+// });
