5ede70e435df9d23a964246dd4273a024b047a34
J3SS3HVA <jesse.leeuwerink@hva.nl>
2024-01-23 11:18:08 +0100

    ver 0.85
    all interactions done
    webpage almost completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,16 @@
 // show schedule section
 const Schedule = document.querySelector('.schedule');
 const ScheduleBtn = document.querySelector('#Schedule-button');
+// show nav menu
+const menuBtn = document.querySelector('.menu')
+const navMenu = document.querySelector('body > article')
 
+// click on button gives the schedule section a class 
 ScheduleBtn.addEventListener('click', function(){
     Schedule.classList.toggle('show-schdule');
+});
+
+// click on button gives the nav menu a class 
+menuBtn.addEventListener('click', function(){
+    navMenu.classList.toggle('show-menu');
 });
