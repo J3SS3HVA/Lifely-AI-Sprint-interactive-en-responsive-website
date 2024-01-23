b56f4629071fa24b7918d21fc4934a3a5d10aff7
J3SS3HVA <jesse.leeuwerink@hva.nl>
2024-01-23 17:24:47 +0100

    0.95
    official beta version

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ const ScheduleBtn = document.querySelector('#Schedule-button');
 // show nav menu
 const menuBtn = document.querySelector('.menu')
 const navMenu = document.querySelector('body > article')
+const body = document.querySelector('body');
 
 // click on button gives the schedule section a class 
 ScheduleBtn.addEventListener('click', function(){
@@ -13,4 +14,5 @@ ScheduleBtn.addEventListener('click', function(){
 // click on button gives the nav menu a class 
 menuBtn.addEventListener('click', function(){
     navMenu.classList.toggle('show-menu');
+    body.classList.toggle('hide-scroll');
 });
